8050cda149e4c0a2aa50667c2dab747976916cb7
Nikunj Dhingra <35530096+Nikunj2102@users.noreply.github.com>
2019-06-05 21:54:30 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DirectoryComponent } from './directory/directory.component';
-
+//Add the array in the imports array
 @NgModule({
   declarations: [
     AppComponent,

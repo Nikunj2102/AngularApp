2c67ffc79e257174e9939ba58bafc555792d4a9b
Nikunj Dhingra <35530096+Nikunj2102@users.noreply.github.com>
2019-06-05 21:53:19 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {Routes,RouterModule, Route} from '@angular/router';
 import { DirectoryComponent } from './directory/directory.component';
 import { HomeComponent } from './home/home.component';
 
-
+//now try using array instead
 const routes: Routes = [
     {path:"directory" , component:DirectoryComponent},
     {path: "directory/:ninja" , component:DirectoryComponent},
